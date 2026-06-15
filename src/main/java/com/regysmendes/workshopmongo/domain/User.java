058122e1dac06d2e8a539619eeb83d51d3903fbb
regys-mendes <regyseduardomendes123@gmail.com>
2026-06-15 19:22:42 -0300

    Conectando ao MongoDB com repository e service

--- src/main/java/com/regysmendes/workshopmongo/domain/User.java
+++ src/main/java/com/regysmendes/workshopmongo/domain/User.java
@@ -1,13 +1,15 @@
 package com.regysmendes.workshopmongo.domain;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Objects;
 
-
+@Document(collection = "user")
 public class User implements Serializable {
 
+    @Id
     private String id;
     private String name;
     private String email;
--- src/main/java/com/regysmendes/workshopmongo/resources/UserResource.java
+++ src/main/java/com/regysmendes/workshopmongo/resources/UserResource.java
@@ -1,26 +1,26 @@
 package com.regysmendes.workshopmongo.resources;
 
 import com.regysmendes.workshopmongo.domain.User;
+import com.regysmendes.workshopmongo.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+
 import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+    @Autowired
+    private UserService service;
+
     @GetMapping
     public ResponseEntity<List<User>> findAll(){
-        User maria = new User("1", "Maria Silva", "maria123@gmail.com");
-        User alex = new User("2", "Alex Green", "alex123@gmail.com");
-        List<User> list = new ArrayList<>();
-        list.addAll(Arrays.asList(maria,alex));
+        List<User> list = service.findAll();
         return ResponseEntity.ok().body(list);
     }
 
